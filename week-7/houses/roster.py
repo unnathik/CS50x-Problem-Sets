a5fe9932615c7a6d1829cc1a05e6c4deba80cb01
Unnathi Kumar <64973394+unnathik@users.noreply.github.com>
2021-02-14 10:41:58 +0530

    Add files via upload

---
+++ week-7/houses/roster.py
@@ -0,0 +1,13 @@
+from cs50 import SQL
+from sys import argv
+
+if len(argv) != 2:
+    print("Incorrect Number of Arguments")
+    exit(1)
+
+db = SQL("sqlite:///students.db")
+entered_house = argv[1]
+rows = db.execute("SELECT * FROM students WHERE house = ? ORDER BY last, first", entered_house)
+for row in rows:
+    first, middle, last, birth = row["first"], row["middle"], row["last"], row["birth"]
+    print(f"{first} {middle + ' ' if middle else ''}{last}, born {birth}")
